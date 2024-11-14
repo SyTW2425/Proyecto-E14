e91c119653363f72cd5e71bd11f43774b7828a7d
franmv21 <alu0101316062@ull.edu.es>
2024-11-14 22:17:41 +0000

    Configurando variables de entorno y script con nodemon

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,29 @@
-import express from 'express';
 import dotenv from 'dotenv';
+import express from 'express';
 
+// Cargar las variables de entorno desde el archivo .env
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+// Middleware que transforma el cuerpo de las peticiones a JSON
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+const DB_URI = process.env.DB_URI;
+
+if (!DB_URI) {
+  console.error(
+    'La URI de conexión a MongoDB no está definida en el archivo .env',
+  );
+} else {
+  console.log('Conectando a MongoDB...');
+}
+
+app.get('/ping', (_, res) => {
+  console.log('Pong!');
+  res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on puerto ${PORT}`);
 });
