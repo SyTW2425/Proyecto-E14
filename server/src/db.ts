f671822f8c0d2bed9180b7f54b8bfe9229d8f3d0
franmv21 <alu0101316062@ull.edu.es>
2024-11-15 16:12:18 +0000

    Añadiendo más campos a los modelos y probando las inserciones en la bdd

--- server/src/db.ts
+++ server/src/db.ts
@@ -3,16 +3,21 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/biblioteca';
+const MONGODB_URI = process.env.MONGODB_URI; // || 'mongodb://localhost:27017/biblioteca';
+
+if (!MONGODB_URI) {
+  throw new Error('La variable de entorno MONGODB_URI no está definida.');
+}
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(MONGODB_URI); // Simplificado sin opciones adicionales
-        console.log('Conexión a MongoDB exitosa');
-    } catch (error) {
-        console.error('Error al conectar a MongoDB:', error);
-        process.exit(1); // Detener la aplicación en caso de error
-    }
+  try {
+    console.log('Iniciando conexión a MongoDB...');
+    await mongoose.connect(MONGODB_URI, { dbName: 'pruebas' }); // Simplificado sin opciones adicionales
+    console.log('Conexión a MongoDB exitosa');
+  } catch (error) {
+    console.error('Error al conectar a MongoDB:', error);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,16 +1,64 @@
 import mongoose, { Schema, Document } from 'mongoose';
+import validator from 'validator';
 
 export interface IUsuario extends Document {
-    nombre: string;
-    correo: string;
-    preferenciasLectura: string[];
+  nombre: string;
+  apellidos: string;
+  username: string;
+  correo: string;
+  preferenciasLectura: string[];
+  //password: string;           Para cuando se implemente la autenticación
 }
 
-const UsuarioSchema: Schema = new Schema({
-    nombre: { type: String, required: true },
-    correo: { type: String, required: true, unique: true },
-    preferenciasLectura: { type: [String], required: true },
-}, { collection: 'user' });
+const UsuarioSchema: Schema = new Schema(
+  {
+    nombre: {
+      type: String,
+      required: [true, 'El nombre es obligatorio'],
+      trim: true,
+      minlength: [1, 'El nombre no puede estar vacío'],
+      maxlength: [100, 'El nombre es demasiado largo'],
+    },
+    apellidos: {
+      type: String,
+      required: [true, 'Los apellidos son obligatorios'],
+      trim: true,
+      minlength: [1, 'Los apellidos no pueden estar vacíos'],
+      maxlength: [70, 'Los apellidos son demasiado largos'],
+    },
+    username: {
+      type: String,
+      required: [true, 'El nombre de usuario es obligatorio'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      minlength: [4, 'El nombre de usuario debe tener al menos 3 caracteres'],
+      maxlength: [20, 'El nombre de usuario es demasiado largo'],
+    },
+    correo: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value: string) {
+        if (!validator.isEmail(value)) {
+          throw new Error('El correo electrónico no es válido');
+        }
+      },
+    },
+    preferenciasLectura: {
+      type: [String],
+      required: true,
+      validate(value: string[]) {
+        if (value.length < 1) {
+          throw new Error('Debes seleccionar al menos un género literario');
+        }
+      },
+    },
+  },
+  { collection: 'User' },
+);
 
-const Usuario = mongoose.model<IUsuario>('user', UsuarioSchema);
+const Usuario = mongoose.model<IUsuario>('User', UsuarioSchema);
 export default Usuario;
