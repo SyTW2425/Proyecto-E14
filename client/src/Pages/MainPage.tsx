1550b8dba61c5d4e68b0e0c66a86f55ab7a90ab6
Fran Martin <alu0101316062@ull.edu.es>
2025-01-03 13:38:38 +0000

    Mejorando página que muestra los libros (falta añadir algunos enlaces a la navbar e implementar la lógica para añadir libros a la biblioteca del usuario)

--- client/src/Pages/MainPage.tsx
+++ client/src/Pages/MainPage.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaPlus, FaBook, FaUser } from "react-icons/fa";
 
-// Define the book type
 interface Book {
   _id: string;
   titulo: string;
@@ -13,18 +12,84 @@ interface Book {
   portada: string;
 }
 
+const FilterDropdown: React.FC<{
+  options: string[];
+  selectedOption: string;
+  onSelect: (value: string) => void;
+}> = ({ options, selectedOption, onSelect }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+  const handleSelect = (value: string) => {
+    onSelect(value);
+    setIsOpen(false);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={dropdownRef}
+      className="relative inline-block text-left w-full sm:w-1/6"
+    >
+      <button
+        onClick={toggleDropdown}
+        className="w-full p-2 border border-green1 rounded bg-white text-gray-700 flex justify-between items-center"
+      >
+        {selectedOption || "All genres"}
+        <span className="ml-2">&#9662;</span>
+      </button>
+      {isOpen && (
+        <ul className="absolute z-10 mt-2 w-full bg-white border border-green1 rounded shadow">
+          <li
+            className="cursor-pointer px-4 py-2 hover:bg-green2 hover:text-white"
+            onClick={() => handleSelect("")}
+          >
+            All genres
+          </li>
+          {options.map((option) => (
+            <li
+              key={option}
+              className="cursor-pointer px-4 py-2 hover:bg-green2 hover:text-white"
+              onClick={() => handleSelect(option)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
 export const MainPage: React.FC = () => {
   const navigate = useNavigate();
   const [books, setBooks] = useState<Book[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [selectedGenre, setSelectedGenre] = useState<string>("");
 
-  // Fetch books from the API
   useEffect(() => {
     const fetchBooks = async () => {
       try {
-        const token = localStorage.getItem("token"); // Retrieve the token
+        const token = localStorage.getItem("token");
         const response = await fetch("http://localhost:4200/libros", {
           headers: {
-            Authorization: `Bearer ${token}`, // Pass the token in the Authorization header
+            Authorization: `Bearer ${token}`,
           },
         });
 
@@ -33,8 +98,7 @@ export const MainPage: React.FC = () => {
         }
 
         const data = await response.json();
-        console.log("Fetched Books:", data); // Debugging: log fetched books
-        setBooks(data); // Update state with fetched books
+        setBooks(data);
       } catch (error) {
         console.error("Error fetching books:", error);
       }
@@ -43,8 +107,21 @@ export const MainPage: React.FC = () => {
     fetchBooks();
   }, []);
 
+  const filteredBooks = books.filter((book) => {
+    const matchesSearch = book.titulo
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase());
+    const matchesGenre = selectedGenre
+      ? book.genero.toLowerCase() === selectedGenre.toLowerCase()
+      : true;
+
+    return matchesSearch && matchesGenre;
+  });
+
+  const genres = Array.from(new Set(books.map((book) => book.genero)));
+
   return (
-    <div className="font-roboto h-screen flex">
+    <div className="min-h-screen bg-green-100 flex">
       {/* Sidebar */}
       <nav className="fixed top-0 left-0 h-screen w-16 bg-green1 text-white flex flex-col justify-center items-center">
         <button
@@ -67,41 +144,61 @@ export const MainPage: React.FC = () => {
         </button>
       </nav>
 
-      {/* Main content */}
-      <div className="ml-16 flex-grow flex flex-col">
-        {/* Top bar */}
-        <header className="fixed w-full h-14 bg-green1 text-white flex items-center justify-between p-3">
-          <h1 className="text-2xl font-bold">Welcome to Bookies!</h1>
-        </header>
-
-        {/* Main content */}
-        <main className="flex-grow p-4 mt-14">
-          {/* <h2 className="text-2xl font-bold mb-6 text-center">
-            Book Collection
-          </h2> */}
-          {/* Book Grid */}
-          <div className="max-w-screen-2xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 rounded-lg bg-gray-100 p-6">
-            {books.map((book) => (
-              <div
-                key={book._id}
-                className="max-w-sm rounded overflow-hidden shadow-lg bg-white"
-              >
-                <img
-                  className="w-47 h-64 object-cover mt-4 mx-auto rounded-lg"
-                  src={book.portada}
-                  alt={`${book.titulo} cover`}
-                />
-                <div className="px-6 py-4">
-                  <h3 className="font-bold text-xl mb-2">{book.titulo}</h3>
-                  <p className="text-gray-700 text-base">By {book.autor}</p>
-                  <p className="text-gray-700 text-base">
-                    <strong>Genre:</strong> {book.genero}
-                  </p>
-                </div>
-              </div>
-            ))}
+      {/* Main Content */}
+      <div className="flex-grow ml-16">
+        <main className="py-4 mt-4">
+          {/* Search and Filter */}
+          <div className="flex flex-col sm:flex-row justify-between items-center p-4 space-y-4 sm:space-y-0 max-w-screen-2xl mx-auto">
+            <input
+              type="text"
+              placeholder="Search by title..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="w-full sm:w-1/2 sm:mb-0 sm:mr-4 p-2 border border-green1 rounded focus:outline-none focus:ring-1 focus:ring-green2"
+            />
+            <FilterDropdown
+              options={genres}
+              selectedOption={selectedGenre}
+              onSelect={setSelectedGenre}
+            />
           </div>
-          ;
+
+          <div className="max-w-screen-2xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 rounded-lg p-6 bg-green-100">
+  {filteredBooks.map((book) => (
+    <div
+      key={book._id}
+      className="relative max-w-sm rounded overflow-hidden bg-green-50 shadow-md"
+    >
+      {/* Book Cover */}
+      <div className="relative group">
+        <img
+          className="w-47 h-96 object-cover rounded-lg mt-4 mx-auto transition-all duration-300 group-hover:blur-[2px]"
+          src={book.portada}
+          alt={`${book.titulo} cover`}
+        />
+        {/* Add Button */}
+        <button
+          onClick={() => console.log(`Added ${book.titulo} to library`)}
+          className="absolute inset-0 flex items-center justify-center bg-transparent text-green1 opacity-[0.01] transition-opacity duration-300 group-hover:opacity-100"
+        >
+          <div className="bg-green1 p-3 rounded-full">
+            <FaPlus size={30} className="text-white" />
+          </div>
+        </button>
+      </div>
+
+      {/* Book Details */}
+      <div className="px-6 py-4">
+        <h3 className="font-bold text-xl mb-2">{book.titulo}</h3>
+        <p className="text-gray-700 text-base">By {book.autor}</p>
+        <p className="text-gray-700 text-base">
+          <strong>Genre:</strong> {book.genero}
+        </p>
+      </div>
+    </div>
+  ))}
+</div>
+
         </main>
       </div>
     </div>
