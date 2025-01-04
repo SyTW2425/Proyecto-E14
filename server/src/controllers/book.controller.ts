9a7bdfeb47af34a45c9d66d61c1975975738fbac
Sebastián André Porto Specht <alu0101494265@ull.edu.es>
2025-01-04 21:13:18 +0000

    Book tests funcionando

--- server/src/controllers/book.controller.ts
+++ server/src/controllers/book.controller.ts
@@ -32,7 +32,7 @@ export const getBookById = async (req: Request, res: Response) => {
     if (!book) return res.status(404).json({ error: 'Book not found' });
     res.status(200).json(book);
   } catch (error) {
-    res.status(500).json({ error: (error as Error).message });
+    res.status(400).json({ error: (error as Error).message });
   }
 };
 
@@ -63,6 +63,6 @@ export const deleteBook = async (req: Request, res: Response) => {
       .status(200)
       .json({ message: 'Book deleted successfully', book: deletedBook });
   } catch (error) {
-    res.status(500).json({ error: (error as Error).message });
+    res.status(400).json({ error: (error as Error).message });
   }
 };
--- server/tests/books.test.js
+++ server/tests/books.test.js
@@ -1,38 +1,53 @@
 import request from 'supertest';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
 import app from '../src/index';
-import { describe, it, expect, beforeAll, afterAll } from '@jest/globals';
+import { describe, it, expect, beforeAll, afterAll, afterEach } from '@jest/globals';
+import jest from 'jest-mock';
 import Libro from '../src/models/book';
 
 describe('Books Endpoints', () => {
+  let token;
+  let mockBook;
+
   beforeAll(async () => {
     process.env.NODE_ENV = 'test'; // Asegura que usamos el entorno de pruebas
     await mongoose.connect(process.env.TEST_MONGODB_URI, {
       dbName: 'tests', // Base de datos de pruebas
     });
+
+    // Generar un token válido
+	  token = jwt.sign(
+    	{ userId: 'testuser', role: 'user' }, // Datos del usuario
+    	process.env.JWT_SECRET, // Clave secreta usada por el servidor
+    	{ expiresIn: '1h' } // Tiempo de expiración
+	  );
+    mockBook = {
+      titulo: "Dune",
+      autor: "Frank Herbert",
+      genero: "Science Fiction",
+      paginas: 412,
+      sinopsis: "A desert planet holds the key to the galaxy's future in this epic tale of power and betrayal.",
+      portada: "https://m.media-amazon.com/images/I/91bNnC0hTFL._AC_UF894,1000_QL80_.jpg"
+    };
   });
 
-  afterAll(async () => {
+  afterEach(async () => {
     const collections = mongoose.connection.collections;
 
     for (const key in collections) {
       await collections[key].deleteMany({}); // Limpia todos los documentos
     }
+  });
 
+  afterAll(async () => {
     await mongoose.disconnect();
     console.log('Conexión a la base de datos de pruebas cerrada');
   });
-
-  describe('POST /books', () => {
+    // Post a book
     it('should add a new book', async () => {
-      const mockBook = {
-        title: 'Sample Book',
-        author: 'Test Author',
-        genre: 'Fiction',
-        year: 2023,
-      };
 
-      const response = await request(app).post('/books').send(mockBook);
+      const response = await request(app).post('/libros').set('Authorization', `Bearer ${token}`).send(mockBook);
 
       expect(response.status).toBe(201);
       expect(response.body).toHaveProperty(
@@ -40,56 +55,96 @@ describe('Books Endpoints', () => {
         'Book added successfully'
       );
       expect(response.body).toHaveProperty('book');
-      expect(response.body.book).toHaveProperty('title', 'Sample Book');
+      expect(response.body.book).toHaveProperty('titulo', 'Dune');
+      expect(response.body.book).toHaveProperty('autor', 'Frank Herbert');
+      expect(response.body.book).toHaveProperty('genero', 'Science Fiction');
+      expect(response.body.book).toHaveProperty('paginas', 412);
+      expect(response.body.book).toHaveProperty('sinopsis', 'A desert planet holds the key to the galaxy\'s future in this epic tale of power and betrayal.');
+      expect(response.body.book).toHaveProperty('portada', 'https://m.media-amazon.com/images/I/91bNnC0hTFL._AC_UF894,1000_QL80_.jpg');
     });
-  });
 
-  describe('GET /books', () => {
+    it('should add a new book', async () => {
+      mockBook.portada = 'mi_portada.jpg';
+      const response = await request(app).post('/libros').set('Authorization', `Bearer ${token}`).send(mockBook);
+      
+      expect(response.status).toBe(400);
+      mockBook.portada = 'https://m.media-amazon.com/images/I/91bNnC0hTFL._AC_UF894,1000_QL80_.jpg';
+    });
+
+    it('should return 400 if Bad Request', async () => {
+      const response = await request(app).post('/libros').set('Authorization', `Bearer ${token}`).send({});
+
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty('error', 'Book validation failed: portada: The cover URL is required, ' +
+                                                    'sinopsis: The synopsis is required, paginas: The number of pages is required, ' +
+                                                    'genero: The genre is required, autor: The author is required, titulo: The title is required');
+    });
+
+    // Get all books
     it('should return all books', async () => {
-      const response = await request(app).get('/books');
+
+      await request(app).post('/libros').set('Authorization', `Bearer ${token}`).send(mockBook);
+
+      const response = await request(app).get('/libros').set('Authorization', `Bearer ${token}`);
 
       expect(response.status).toBe(200);
       expect(Array.isArray(response.body)).toBe(true);
+      expect(response.body.length).toBe(1);
+      expect(response.body[0]).toHaveProperty('titulo', 'Dune');
+      expect(response.body[0]).toHaveProperty('autor', 'Frank Herbert');
+      expect(response.body[0]).toHaveProperty('genero', 'Science Fiction');
+      expect(response.body[0]).toHaveProperty('paginas', 412);
+      expect(response.body[0]).toHaveProperty('sinopsis', 'A desert planet holds the key to the galaxy\'s future in this epic tale of power and betrayal.');
+      expect(response.body[0]).toHaveProperty('portada', 'https://m.media-amazon.com/images/I/91bNnC0hTFL._AC_UF894,1000_QL80_.jpg');
     });
-  });
 
-  describe('GET /books/:id', () => {
+    it('should return 500 if Internal Server Error', async () => {
+      jest.spyOn(Libro, 'find').mockRejectedValueOnce(new Error('Database error'));
+    
+      const response = await request(app).get('/libros').set('Authorization', `Bearer ${token}`);
+    
+      expect(response.status).toBe(500);
+      expect(response.body).toHaveProperty('error', 'Database error');
+    });
+
+    // Get a book by ID
     it('should return a book by ID', async () => {
-      const mockBook = new Libro({
-        title: 'Sample Book',
-        author: 'Test Author',
-        genre: 'Fiction',
-        year: 2023,
-      });
-      await mockBook.save();
 
-      const response = await request(app).get(`/books/${mockBook._id}`);
+      const post_response = await request(app).post('/libros').set('Authorization', `Bearer ${token}`).send(mockBook);
+
+      const mockBookID =  post_response.body.book._id;
+
+      const response = await request(app).get(`/libros/${mockBookID}`).set('Authorization', `Bearer ${token}`);
 
       expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty('title', 'Sample Book');
+      expect(response.body).toHaveProperty('titulo', 'Dune');
     });
 
     it('should return 404 if book not found', async () => {
-      const response = await request(app).get('/books/60d5f99cfc13ae2d10000000');
+      const response = await request(app).get('/libros/60d5f99cfc13ae2d10000000').set('Authorization', `Bearer ${token}`);
 
       expect(response.status).toBe(404);
       expect(response.body).toHaveProperty('error', 'Book not found');
     });
-  });
 
-  describe('PUT /books/:id', () => {
+    it('should return 400 if Bad Request', async () => {
+      const response = await request(app).get('/libros/1').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty('error', 'Cast to ObjectId failed for value \"1\" (type string) at path \"_id\" for model \"Book\"');
+    });
+
+    // Update a book
     it('should update a book', async () => {
-      const mockBook = new Libro({
-        title: 'Sample Book',
-        author: 'Test Author',
-        genre: 'Fiction',
-        year: 2023,
-      });
-      await mockBook.save();
+      
+      const post_response = await request(app).post('/libros').set('Authorization', `Bearer ${token}`).send(mockBook);
 
-      const updatedData = { title: 'Updated Book Title' };
+      const mockBookID =  post_response.body.book._id;
+
+      const updatedData = { titulo: 'Updated Book Title' };
       const response = await request(app)
-        .put(`/books/${mockBook._id}`)
+        .put(`/libros/${mockBookID}`)
+        .set('Authorization', `Bearer ${token}`)
         .send(updatedData);
 
       expect(response.status).toBe(200);
@@ -97,30 +152,37 @@ describe('Books Endpoints', () => {
         'message',
         'Book updated successfully'
       );
-      expect(response.body.book).toHaveProperty('title', 'Updated Book Title');
+      expect(response.body.book).toHaveProperty('titulo', 'Updated Book Title');
     });
 
     it('should return 404 if book not found', async () => {
       const response = await request(app)
-        .put('/books/60d5f99cfc13ae2d10000000')
-        .send({ title: 'Non-existent Book' });
+        .put('/libros/60d5f99cfc13ae2d10000000')
+        .set('Authorization', `Bearer ${token}`)
+        .send({ titulo: 'Non-existent Book' });
 
       expect(response.status).toBe(404);
       expect(response.body).toHaveProperty('error', 'Book not found');
     });
-  });
 
-  describe('DELETE /books/:id', () => {
+    it('should return 400 if bad request', async () => {
+      const response = await request(app)
+        .put('/libros/1')
+        .set('Authorization', `Bearer ${token}`)
+        .send({ titulo: 'Non-existent Book' });
+
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty('error', 'Cast to ObjectId failed for value \"1\" (type string) at path \"_id\" for model \"Book\"');
+    });
+
+    // Delete a book
     it('should delete a book', async () => {
-      const mockBook = new Libro({
-        title: 'Sample Book',
-        author: 'Test Author',
-        genre: 'Fiction',
-        year: 2023,
-      });
-      await mockBook.save();
+      
+      const post_response = await request(app).post('/libros').set('Authorization', `Bearer ${token}`).send(mockBook);
 
-      const response = await request(app).delete(`/books/${mockBook._id}`);
+      const mockBookID =  post_response.body.book._id;
+
+      const response = await request(app).delete(`/libros/${mockBookID}`).set('Authorization', `Bearer ${token}`);
 
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty(
@@ -130,10 +192,18 @@ describe('Books Endpoints', () => {
     });
 
     it('should return 404 if book not found', async () => {
-      const response = await request(app).delete('/books/60d5f99cfc13ae2d10000000');
+      
+      const response = await request(app).delete('/libros/60d5f99cfc13ae2d10000000').set('Authorization', `Bearer ${token}`);
 
       expect(response.status).toBe(404);
       expect(response.body).toHaveProperty('error', 'Book not found');
     });
-  });
+
+    it('should return 400 if Bad Request', async () => {
+      
+      const response = await request(app).delete('/libros/1').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty('error', 'Cast to ObjectId failed for value \"1\" (type string) at path \"_id\" for model \"Book\"');
+    });
 });
